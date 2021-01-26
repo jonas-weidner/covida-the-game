1ad01de67db095aba2fc6f6883d8c61f4f5c7bc3
Jonas Weidner <jonas@tuutr.io>
2021-01-26 09:49:52 +0100

    Added a language switcher

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-    <router-view />
+    <div>
+        <router-view />
+        <language-switcher />
+    </div>
 </template>
 
 <script>
-
-export default { name: "App" };
+import LanguageSwitcher from "@/components/languageSwitcher/LanguageSwitcher";
+export default {
+    name: "App",
+    components: { LanguageSwitcher }
+};
 </script>
 
 <style>
---
+++ src/components/languageSwitcher/LanguageSwitcher.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="language-switcher-wrapper shadow-2xl flex items-center space-x-2 rounded-tr-lg">
+        <div class="cursor-pointer w-5 h-5 rounded-full overflow-hidden"
+             @click="$i18n.locale ='en'">
+            <img src="@/assets/united-kingdom.svg" />
+        </div>
+        <div class="cursor-pointer w-5 h-5 rounded-full overflow-hidden"
+             @click="$i18n.locale ='de'">
+            <img src="@/assets/germany.svg" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class LanguageSwitcher extends Vue {
+
+}
+</script>
+
+<style scoped>
+.language-switcher-wrapper {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    padding: 10px;
+    background-color: white;
+}
+</style>
